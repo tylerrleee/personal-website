57c7bcdee7b34004c4996c5474878b3b498101c7
tylerrleee <160276402+tylerrleee@users.noreply.github.com>
2025-09-01 16:11:45 -0400

    august

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Import the new CSS file
-import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('patternCanvas')!).render(
+  <React.StrictMode>
+  </React.StrictMode>,
+);
